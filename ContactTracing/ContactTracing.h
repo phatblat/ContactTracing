30bec1fb84d06c0ab4b8e1e64b086c2235282fc5
Ben Chatelain <ben@octop.ad>
2020-04-12 14:14:51 -0600

    Initial Commit

---
+++ ContactTracing/ContactTracing.h
@@ -0,0 +1,19 @@
+//
+//  ContactTracing.h
+//  ContactTracing
+//
+//  Created by Ben Chatelain on 4/12/20.
+//  Copyright © 2020 Log Dash G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContactTracing.
+FOUNDATION_EXPORT double ContactTracingVersionNumber;
+
+//! Project version string for ContactTracing.
+FOUNDATION_EXPORT const unsigned char ContactTracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactTracing/PublicHeader.h>
+
+
